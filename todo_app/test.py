bf81c1b233cae12addea3c545c3276ab50ea83f5
codingsake <thinthazin.chris@gmail.com>
2022-01-24 17:51:34 -0500

    added todo_app project

---
+++ todo_app/test.py
@@ -0,0 +1,24 @@
+from app import app
+
+def test1():
+    
+    """
+    This function test that the flask application has a 
+    correct response code when the application goes live 
+    """
+    response = app.test_client().get("/")
+    assert response.status_code == 200
+
+
+def test2():
+    """A dummy docstring"""
+    response = app.test_client().get("/edit")
+    assert response.status_code == 200
+    
+
+def test3():
+    """this fucntion check whether these text exits in the /edit browser"""
+    response = app.test_client().get("/edit")
+    assert b"To Do App" in response.data
+    assert b"To Do Title" in response.data
+    assert b"Add" in response.data
